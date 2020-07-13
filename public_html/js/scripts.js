284171ba35e7d3fe030aef9656f8b6f41aeec52f
diogenesjup <diogenesjunior.ti@gmail.com>
2020-07-13 11:47:08 -0300

    atualizações 13 de julho

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -1,21 +1,111 @@
                         $( window ).ready(function() {
 
                                 var owlDeCarros = $('#owlDeCarros').owlCarousel({
-                                        loop:false,
+                                        loop:true,
                                         margin:20,
                                         items: 5,
                                         autoplay: true,
                                         center: true,
                                         navText: [
-                                            '<img src="images/left.png" alt="Banner anterior">',
-                                            '<img src="images/right.png" alt="Próximo Banner">'
+                                            '<img src="images/esquerda.png" alt="Página anterior">',
+                                            '<img src="images/direita.png" alt="Próxima página">'
                                         ], 
-                                        //navContainer: '.custom-nav-banner',
+                                        navContainer: '.custom-nav-banner',
                                         autoplayTimeout:2500,
                                         //dotsContainer: '#carousel-custom-dots',
                                         autoplayHoverPause:true,
                                         //animateIn: 'fadeIn', // add this
                                         //animateOut: 'fadeOut', // and this
-                                       
+                                       responsive:{
+                                            0:{
+                                                items:1.5,
+                                                margin:5,
+                                                loop:true,
+                                            },
+                                            600:{
+                                                items:2.5,
+                                                margin:5,
+                                                loop:true,
+                                            },
+                                            1000:{
+                                                items:4.5,
+                                                margin:5,
+                                                loop:true,
+                                            },
+                                            1200:{
+                                                loop:true,
+                                                margin:20,
+                                                items: 5,
+                                                autoplay: true,
+                                                center: true,
+                                                navText: [
+                                                    '<img src="images/esquerda.png" alt="Página anterior">',
+                                                    '<img src="images/direita.png" alt="Próxima página">'
+                                                ], 
+                                                navContainer: '.custom-nav-banner',
+                                                autoplayTimeout:2500,
+                                                //dotsContainer: '#carousel-custom-dots',
+                                                autoplayHoverPause:true,
+                                            }
+                                        }
                                 });
+
+
+                                var owlDeCarrosGaleria = $('#owlDeCarrosGaleria').owlCarousel({
+                                        loop:true,
+                                        margin:5,
+                                        items: 1,
+                                        autoplay: true,
+                                        center: true,
+                                        autoplayTimeout:2500,
+                                        //dotsContainer: '#carousel-custom-dots',
+                                        autoplayHoverPause:true,
+                                        //animateIn: 'fadeIn', // add this
+                                        //animateOut: 'fadeOut', // and this
+                                       responsive:{
+                                            0:{
+                                                items:1.5,
+                                                margin:5,
+                                                loop:true,
+                                            },
+                                            600:{
+                                                items:2.5,
+                                                margin:5,
+                                                loop:true,
+                                            },
+                                            1000:{
+                                                items:4.5,
+                                                margin:5,
+                                                loop:true,
+                                            },
+                                            1200:{
+                                                loop:true,
+                                                margin:5,
+                                                items: 2.5,
+                                                autoplay: true,
+                                                center: true,
+                                                autoplayTimeout:2500,
+                                                //dotsContainer: '#carousel-custom-dots',
+                                                autoplayHoverPause:true,
+                                            }
+                                        }
+                                });
+
+                                
                         }); 
+
+
+
+// MANTER O MENU FIXO MESMO QUANDO ROLAR A PÁGINA
+jQuery(document).ready(function ($) {
+  $(window).scroll(function(){
+    var sticky = $('header.fixo'),
+        scroll = $(window).scrollTop();  
+    if (scroll < 250){ 
+        $('header.fixo').css("top","-1000%");
+     }
+    if (scroll > 250){ 
+        $('header.fixo').css("top","0px");
+     }
+  });
+});  
